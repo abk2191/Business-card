75ee5fb97437cbc32ddd8b1452e1a3c375cd79b5
Abhishek <abhishek.kabi.21@gmail.com>
2025-10-17 03:51:14 +0530

    Added new features

--- src/secondcard.jsx
+++ src/secondcard.jsx
@@ -23,14 +23,55 @@ function Secondcard({ handleFollow, handleSendMessage }) {
           textAlign: "center",
         }}
       >
-        Vellore Institute of Technology (VIT){" "}
+        <i class="fa-solid fa-building-columns"></i> Vellore Institute of
+        Technology (VIT){" "}
       </h3>
       <p style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
-        Course: B.Tech - Information Technology & Engineering.
+        <i class="fa-solid fa-graduation-cap"></i> Course: B.Tech - Information
+        Technology & Engineering.
       </p>
+
       <p style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
-        Grade: 6.68
+        <i class="fa-solid fa-star-half-stroke"></i> Grade: 6.68
       </p>
+      <hr
+        style={{
+          border: "none",
+          height: "1px",
+          backgroundColor: "#ccc",
+          margin: "20px 0",
+        }}
+      />
+      <h3 style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
+        Tech Skills:
+      </h3>
+      <p>
+        <i class="fa-brands fa-react"></i> React
+      </p>
+      <p>
+        <i class="fa-brands fa-square-js"></i> Javascript
+      </p>
+      <p>
+        <i class="fa-brands fa-html5"></i> HTML 5
+      </p>
+      <p>
+        <i class="fa-brands fa-css3-alt"></i> CSS
+      </p>
+      <hr
+        style={{
+          border: "none",
+          height: "1px",
+          backgroundColor: "#ccc",
+          margin: "20px 0",
+        }}
+      />
+      <button
+        className="addfrndbutton"
+        style={{ backgroundColor: "gold", color: "black" }}
+      >
+        <i class="fa-solid fa-download" style={{ padding: "8px" }}></i>
+        Download My Resume
+      </button>
     </div>
   );
 }
