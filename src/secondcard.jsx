c34557f9a5775028348f0fdb91054789975d535a
Abhishek <abhishek.kabi.21@gmail.com>
2025-10-17 03:33:48 +0530

    Added new features

--- src/secondcard.jsx
+++ src/secondcard.jsx
@@ -4,9 +4,9 @@ function Secondcard({ handleFollow, handleSendMessage }) {
   return (
     <div className="container">
       <div className="profilePicture">
-        <h1 style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
+        <h3 style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
           Education Qualification
-        </h1>
+        </h3>
       </div>
       <hr
         style={{
@@ -16,11 +16,17 @@ function Secondcard({ handleFollow, handleSendMessage }) {
           margin: "20px 0",
         }}
       />
-      <h3 style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
+      <h3
+        style={{
+          color: "white",
+          fontFamily: "Inter, sans-serif",
+          textAlign: "center",
+        }}
+      >
         Vellore Institute of Technology (VIT){" "}
       </h3>
       <p style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
-        Course: Information Technology & Engineering
+        Course: B.Tech - Information Technology & Engineering.
       </p>
       <p style={{ color: "white", fontFamily: "Inter, sans-serif" }}>
         Grade: 6.68
